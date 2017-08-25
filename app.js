428a9bfcf78054593f81a5ffa7df172c7a8f9563
John Gonzales <jgonzales394@gmail.com>
2017-08-25 03:21:42 -0600

    Added a kappa cmd

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var tmi = require('tmi.js');
+var consts = require('./kappas');
+
+var users = ["takarita324"];
+var bc = users[0];
 
 var options ={
   options:{
@@ -12,7 +16,7 @@ var options ={
     username: "keonibot",
     password: "oauth:ov6hqddafha7mmksmgj4woprlhyrxc"
   },
-  channels:["jgonzales394"]
+  channels: users
 };
 
 var client = new tmi.client(options);
@@ -22,4 +26,14 @@ client.on('connected', function(address,port){
   console.log("Address: "+ address +"Port: "+ port);
 });
 
+client.on("chat",function(channel, userstate, message, self){
+  if(self) return;
 
+  if(message=="!kappa" || message=="!Kappa"){
+    if(userstate['display-name']==bc || userstate['display-name']=="jgonzales394"){
+      for (var i = 0; i < 5; i++) {
+        client.say(channel,consts.kappas);
+      }
+    }
+  }  
+});
---
+++ kappas.js
@@ -0,0 +1,3 @@
+module.exports = { 
+  kappas : "KappaCool Zappa KappaRoss Kappa Zappa Keepo Keepo KappaWealth KappaClaus KappaRoss KappaWealth KappaWealth Kappa Zappa KappaCool Kappa KaRappa Keepo KappaPride KappaPride KappaPride KaRappa KappaClaus KappaClaus KappaClaus KappaCool Kappa Zappa KappaWealth KaRappa KappaCool Keepo KappaRoss KaRappa KappaPride KappaRoss"
+};
